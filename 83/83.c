f8bf5bb08f0416aed10601fa9c519b4d7849ce13
liaoliao <noliaoliao@gmail.com>
2015-04-23 10:38:36 +0800

    Remove Duplicates from Sorted List

---
+++ 83/83.c
@@ -0,0 +1,28 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *deleteDuplicates(struct ListNode *head) {
+    struct ListNode *pre = head;
+    struct ListNode *cur = NULL;
+    if(pre){
+        cur = pre->next;
+    }
+    while(cur != NULL)
+    {
+        if(pre->val == cur->val)
+        {
+            pre->next = cur->next;
+            //free(cur);
+            cur = pre->next;
+        }
+        else{
+            pre = pre->next;
+            cur = cur->next;
+        }
+    }
+    return head;
+}

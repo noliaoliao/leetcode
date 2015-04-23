20b0537bb0b8b5bd7e03d72f1877df85d1c18098
liaoliao <noliaoliao@gmail.com>
2015-04-23 10:51:09 +0800

    Remove Linked List Elements

---
+++ 203/203.c
@@ -0,0 +1,23 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    struct ListNode *pRetHead = (struct ListNode*)malloc(sizeof(struct ListNode));
+    struct ListNode *pRetCur = pRetHead;
+    struct ListNode *pWalk = head;
+    //pRetHead->next = NULL;
+
+    while(pWalk != NULL){
+        if(pWalk->val != val){
+            pRetCur->next = pWalk;
+            pRetCur = pRetCur->next;
+        }
+        pWalk = pWalk->next;
+    }
+    pRetCur->next = NULL;
+    return pRetHead->next;
+}

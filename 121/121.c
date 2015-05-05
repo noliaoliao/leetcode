deda2b62a13bfad930fad3334e0f4e4225c50f10
liaoliao <noliaoliao@gmail.com>
2015-05-05 21:55:39 +0800

    Best Time to Buy and Sell Stock

---
+++ 121/121.c
@@ -0,0 +1,14 @@
+﻿//遍历一遍数组
+//维护i之前的最小值及最大的差值
+//差值最大值即所能获得的最大利润
+int maxProfit(int* prices, int pricesSize) {
+    int i = 0;
+    int minPrice = prices[0];
+    int maxPrice = 0;
+    for(i = 0; i < pricesSize; i++){
+        if(minPrice > prices[i]) minPrice = prices[i];
+        if(prices[i]-minPrice > maxPrice) maxPrice = prices[i]-minPrice;
+    }
+    
+    return maxPrice;
+}

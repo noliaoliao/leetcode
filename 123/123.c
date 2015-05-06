e4c9102a1fa42ab0a9c2079cfcfca5d7c8a3a8c3
liaoliao <noliaoliao@gmail.com>
2015-05-06 18:33:33 +0800

    Best Time to Buy and Sell Stock III

---
+++ 123/123.c
@@ -0,0 +1,29 @@
+﻿int maxProfit(int* prices, int pricesSize) {
+    //lMaxProfit[]保存0~i的最大利润
+    //rMaxProfit[]保存i~pricesSize-1的最大利润
+    int *lMaxProfit = (int *)malloc(sizeof(int)*(pricesSize+1));
+    int *rMaxProfit = (int *)malloc(sizeof(int)*(pricesSize+1));
+
+    int minPrice = prices[0];
+    lMaxProfit[0] = 0;
+    for(int i = 1; i < pricesSize; i++){
+        lMaxProfit[i] = prices[i]-minPrice > lMaxProfit[i-1] ? prices[i]- minPrice : lMaxProfit[i-1];
+        minPrice = prices[i] < minPrice ? prices[i] : minPrice;
+    }
+
+    int maxPrice = prices[pricesSize-1];
+    rMaxProfit[pricesSize-1] = 0;
+    for(int i = pricesSize-2; i >= 0; i--){
+        rMaxProfit[i] = maxPrice-prices[i] > rMaxProfit[i+1] ? maxPrice-prices[i] : rMaxProfit[i+1];
+        maxPrice = prices[i] > maxPrice ? prices[i] : maxPrice;
+    }
+
+    //进行搜索
+    int nMaxProfit = 0;
+    for(int i = 1; i < pricesSize; i++){
+        if(lMaxProfit[i]+rMaxProfit[i] > nMaxProfit){
+            nMaxProfit = lMaxProfit[i]+rMaxProfit[i];
+        }
+    }
+    return nMaxProfit;
+}

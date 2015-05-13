99e89d7fe412a5b4a41a2e515c4164e68e4707de
liaoliao <noliaoliao@gmail.com>
2015-05-13 17:34:46 +0800

    Merge Intervals

---
+++ 56/56.c
@@ -0,0 +1,45 @@
+﻿/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+//自定义比较函数
+bool mycmp(Interval s1, Interval s2){
+    return s1.start < s2.start;
+}
+
+class Solution {  
+public:  
+    vector<Interval> merge(vector<Interval> &intervals) {  
+        vector<Interval> vRet;  
+        sort(intervals.begin(),intervals.end(),mycmp);  
+        int left = -1;
+        int right = -1;
+        int nlen = intervals.size();  
+        for(int i = 0; i < nlen; i++)  
+        {        
+            //判断区间
+            if(intervals[i].start<=right)  
+            {  
+                right=max(right,intervals[i].end);  
+            }  
+            else  
+            {  
+                if(left != -1){
+                    vRet.push_back(Interval(left,right));
+                }
+                left = intervals[i].start;  
+                right = intervals[i].end;  
+            }  
+        }
+        //最后一个元素也要压入
+        if(left != -1){
+            vRet.push_back(Interval(left,right));
+        }
+        return vRet;  
+    }  
+};

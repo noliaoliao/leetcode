82532ed4ca8472790be67caaa154e71dd7a8fd1e
liaoliao <noliaoliao@gmail.com>
2015-05-14 11:39:21 +0800

    Longest Consecutive Sequence

---
+++ 128/128.c
@@ -0,0 +1,30 @@
+﻿//最长连续序列
+//利用map实现,还不是最优的
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int nMaxLong = 0;
+        map<int, int> htab;
+        for(int i = 0; i < nums.size(); i++){
+           htab[nums[i]] = 1;
+        }
+        for(int i = 0; i < nums.size(); i++){
+            int tmp = 1;
+            for(int j = 1; j < nums.size(); j++){
+                //int x = htab[nums[i]+2];
+                if(htab[nums[i]+j] != 1) break;
+                tmp++;
+                htab[nums[i]+j] = 0;
+            }
+            for(int j = 1; j < nums.size(); j++){
+                if(htab[nums[i]-j] != 1) break;
+                tmp++;
+                htab[nums[i]-j] = 0;
+            }
+            if(tmp > nMaxLong) nMaxLong = tmp;
+        }
+
+        return nMaxLong;
+    }
+};

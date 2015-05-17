c935ee85cb478db1020b22bfb6bb5b99e9ca746e
liaoliao <noliaoliao@gmail.com>
2015-05-17 19:41:23 +0800

    Path Sum II

---
+++ 113/113.c
@@ -0,0 +1,23 @@
+﻿//递归遍历即可
+
+class Solution {
+public:
+    void getAllPath(TreeNode *root, int sum, vector<vector<int>> &vAllPath, vector<int> vEachPath){
+        if(root == NULL) return;
+        vEachPath.push_back(root->val);
+        if(root->val == sum && root->left == NULL && root->right == NULL){
+            vAllPath.push_back(vEachPath);
+        }else{
+            getAllPath(root->left,sum-root->val,vAllPath,vEachPath);
+            getAllPath(root->right,sum-root->val,vAllPath,vEachPath);
+        }
+
+    }
+
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> vRet;
+        vector<int> vEach;
+        getAllPath(root,sum,vRet,vEach);
+        return vRet;
+    }
+};

728b3e2fcc6695cd1b855021f03494bca2c13d38
liaoliao <noliaoliao@gmail.com>
2015-05-17 21:48:32 +0800

    Convert Sorted Array to Binary Search Tree

---
+++ 108/108.c
@@ -0,0 +1,33 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode *createBST(struct TreeNode *root, int *nums, int start, int end){
+    if(nums == NULL || start > end) return NULL;
+    //处理长度为1的情况
+    if(start == end){
+        root->val = nums[start];
+        root->left = NULL;
+        root->right = NULL;
+        return root;
+    }
+    //递归进行
+    int mid = (start+end)/2;
+    root->val = nums[mid];
+    root->left = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    root->right = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    root->left = createBST(root->left,nums,start,mid-1);
+    root->right = createBST(root->right,nums,mid+1,end);
+
+    return root;
+}
+
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
+    if(nums == NULL || numsSize == 0) return NULL;
+    struct TreeNode *head = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+    return createBST(head,nums,0,numsSize-1);
+}

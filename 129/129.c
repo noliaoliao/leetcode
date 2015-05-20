3469ab2a6ae144619167465a97a2227ffa5bf082
liaoliao <noliaoliao@gmail.com>
2015-05-20 21:31:20 +0800

    Sum Root to Leaf Numbers

---
+++ 129/129.c
@@ -0,0 +1,19 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int sumTreeNum(struct TreeNode* root, int nPre){
+    if(root == NULL) return 0;
+    if(root->left == NULL && root->right == NULL) return nPre*10+root->val;
+    //左右子树之和
+    return sumTreeNum(root->left,nPre*10+root->val) + sumTreeNum(root->right,nPre*10+root->val);
+}
+
+int sumNumbers(struct TreeNode* root) {
+    if(root == NULL) return 0;
+    return sumTreeNum(root,0);
+}

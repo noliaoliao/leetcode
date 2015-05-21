371a2c377ba17233ec790bf6799d64b82275956c
liaoliao <noliaoliao@gmail.com>
2015-05-21 17:34:41 +0800

    Populating Next Right Pointers in Each Node

---
+++ 116/116.c
@@ -0,0 +1,25 @@
+﻿/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  struct TreeLinkNode *left, *right, *next;
+ * };
+ *
+ */
+void connect(struct TreeLinkNode *root) {
+    if(root == NULL) return ;
+    struct TreeLinkNode *pCur = root;
+    for(;;){
+        struct TreeLinkNode *pHead = pCur->left;
+        while(pCur != NULL){
+            //满二叉树,左右孩子同时为空或非空
+            if(pCur->left != NULL){
+                pCur->left->next = pCur->right;
+                pCur->right->next = pCur->next == NULL ? NULL : pCur->next->left;
+            }
+            pCur = pCur->next;
+        }
+        pCur = pHead;
+        if(pHead == NULL) break;
+    }
+}

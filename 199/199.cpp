8f744af05d35a5ebad23863b30e9bd417a539e09
liaoliao <noliaoliao@gmail.com>
2015-05-21 11:00:46 +0800

    Binary Tree Right Side View

---
+++ 199/199.cpp
@@ -0,0 +1,36 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> vRightSideView;
+        if(root == NULL) return vRightSideView;
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while(!q.empty()){
+            int nQueueSize = q.size();
+            //每层的最后一个节点压入vector
+            vRightSideView.push_back(q.back()->val);
+            for(int i = 0; i < nQueueSize; i++){
+                TreeNode *pCur = q.front();
+                q.pop();
+                if(pCur->left != NULL){
+                    q.push(pCur->left);
+                }
+                if(pCur->right != NULL){
+                    q.push(pCur->right);
+                }
+            }
+        }
+
+        return vRightSideView;
+    }
+};

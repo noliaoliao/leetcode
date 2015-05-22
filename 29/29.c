78d755ee59b6bf4acde19a5d14c874b985a04f11
liaoliao <noliaoliao@gmail.com>
2015-05-22 15:27:10 +0800

    Divide Two Integers

---
+++ 29/29.c
@@ -0,0 +1,24 @@
+﻿int divide(int dividend, int divisor){  
+    if(divisor == 0 || (dividend == INT_MIN && divisor == -1)){
+        return INT_MAX;
+    }
+    if(dividend == divisor){
+        return 1;
+    }
+    if(dividend == 0 || divisor == INT_MIN){
+        return 0;
+    }
+    unsigned a = abs(dividend);
+    unsigned b = abs(divisor);
+    int result = 0;
+
+    while (a >= b){  
+        int count = 0;  
+        while (a >= b<<count){  
+            result += 1<<count;  
+            a -= b<<count;  
+            count++;  
+        }             
+    }  
+    return (dividend > 0) ^ (divisor > 0) ? -result : result;  
+} 

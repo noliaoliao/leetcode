aa0cd4cf60573ce28a53edc6d80ddd1e366eef45
liaoliao <noliaoliao@gmail.com>
2015-05-22 11:10:45 +0800

    ZigZag Conversion

---
+++ 6/6.c
@@ -0,0 +1,21 @@
+﻿char* convert(char* s, int numRows) {
+    int len = strlen(s);
+    if(len <= numRows || numRows == 1) return s;
+    char *ret = (char *)malloc(sizeof(char)*(len+1));
+
+    //依次生成每一层
+    int nRetIndex = 0;
+    for(int i = 0; i < numRows; i++){
+        int index = i;
+        int inc = i*2;
+        while(index < len){
+            ret[nRetIndex++] = s[index];
+            int nAdd = (numRows-1)*2 - inc;
+            inc = (nAdd == 0 ? (numRows-1)*2 : nAdd);
+            index += inc;
+        }
+    }
+    ret[len] = '\0';
+    
+    return ret;
+}

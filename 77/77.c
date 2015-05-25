336cb4fb33ac46323c349a45b55ce39f7385534b
liaoliao <noliaoliao@gmail.com>
2015-05-25 19:40:10 +0800

    Combinations

---
+++ 77/77.c
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+   void getAllCombine(int start, int end, int k, vector<int> vEach, vector<vector<int>> &vAll){
+        if(vEach.size() == k){
+            vAll.push_back(vEach);
+            return;
+        }
+        for(int i = start; i < end; i++){
+            //vEach.clear();
+            vEach.push_back(i);
+            getAllCombine(i+1,end,k,vEach,vAll);
+            vEach.pop_back();
+        }
+    } 
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> vRet;
+        vector<int> vEach;
+        getAllCombine(1,n+1,k,vEach,vRet);
+        return vRet;
+    }
+};

e05269bf0b5e44084e5543caaea3b1663608e8a2
liaoliao <noliaoliao@gmail.com>
2015-05-26 11:23:21 +0800

    Contains Duplicate

---
+++ 217/217.cpp
@@ -0,0 +1,11 @@
+﻿class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> htab;
+        for(int i = 0; i < nums.size(); i++){
+            if(htab.find(nums[i]) != htab.end()) return true;
+            htab.insert(nums[i]);
+        }
+        return false;   
+    }
+};

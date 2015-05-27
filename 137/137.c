8201e56a50f9abcce524f7a99ec96487f1276ca7
liaoliao <noliaoliao@gmail.com>
2015-05-27 16:49:07 +0800

    Single Number II

---
+++ 137/137.c
@@ -0,0 +1,15 @@
+﻿int singleNumber(int* nums, int numsSize) {
+    int ans = 0;
+    //32位依次处理
+    for(int i = 0; i < 32; i++){
+        int count = 0;
+        int offset = 1 << i;
+        for(int j = 0; j < numsSize; j++){
+            if(nums[j] & offset) count++;
+        }
+        //模3就是单个数对应的位
+        if(count%3) ans |= offset;
+    }
+
+    return ans;
+}

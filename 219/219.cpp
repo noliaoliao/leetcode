9457c82aadad5c6c5932329b96dc4250a50c61c1
liaoliao <noliaoliao@gmail.com>
2015-05-29 16:58:41 +0800

    Contains Duplicate II

---
+++ 219/219.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> hashmap;
+        unordered_map<int,int>::iterator it;
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            it = hashmap.find(nums[i]);
+            if(it != hashmap.end()){
+                if(i - it->second <= k) count++;
+            }
+            hashmap[nums[i]] = i;
+        }
+
+        return count == 1;
+    }
+};

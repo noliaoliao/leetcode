230b15e024a8f8b5f3349029c3eb9d30c0010fa9
liaoliao <noliaoliao@gmail.com>
2015-06-05 15:04:32 +0800

    Largest Number

---
+++ 179/179.cpp
@@ -0,0 +1,31 @@
+﻿bool cmp(string str1, string str2){
+    string s1 = str1+str2;
+    string s2 = str2+str1;
+
+    return s1.compare(s2) > 0;
+}
+
+class Solution{
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> v;
+        for(int i = 0; i < nums.size(); i++){
+            //将int转换为string
+            char szBuf[32] = {0};
+            sprintf(szBuf,"%d",nums[i]);
+            string s(szBuf);
+            v.push_back(s);
+        }
+        //排序,如果str1+str2 > str2+str1,则str1>str2
+        sort(v.begin(),v.end(),cmp);
+        
+        string res;
+        for(int i = 0; i < v.size(); i++){
+            res += v[i];
+        }
+        //去掉前导0
+        int cur = 0;
+        while(res[cur] == '0' && cur < res.length()-1) cur++;
+        return res.substr(cur,res.length());
+    }
+};

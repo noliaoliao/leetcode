c926d66d232dc7ed2a0a4b333a7b3654668c04ec
liaoliao <noliaoliao@gmail.com>
2015-06-08 21:32:33 +0800

    Count Complete Tree Nodes

---
+++ 222/222.cpp
@@ -0,0 +1,20 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int countNodes(struct TreeNode* root) {
+    if(root == NULL) return 0;
+    int nLeft = 0;
+    for(struct TreeNode* p = root->left; p != NULL; p = p->left) nLeft++;
+    int nRight = 0;
+    for(struct TreeNode* p = root->right; p != NULL; p = p->right) nRight++;
+    
+    //满二叉树
+    if(nLeft == nRight) return (1<<(nRight+1))-1;
+    if(nRight == 0) return 2;
+    return countNodes(root->left)+countNodes(root->right)+1;
+}

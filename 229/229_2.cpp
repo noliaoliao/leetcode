6ab15eb55786cc1bd72934a598bd21ad9028dcfb
liaoliao <noliaoliao@gmail.com>
2015-06-29 15:45:40 +0800

    Majority Element II

---
+++ 229/229_2.cpp
@@ -0,0 +1,28 @@
+﻿class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        //统计个数
+        //O(n)
+        unordered_map<int,int> hmap;
+        for(int i = 0; i < nums.size(); i++){
+            if(hmap.find(nums[i]) != hmap.end()){
+                hmap[nums[i]]++;
+            }else{
+                hmap[nums[i]] = 1;
+            }
+        }
+
+        vector<int> ret;
+        unordered_set<int> hset;
+        //找出不重复的大于n/3的元素
+        //O(n)
+        for(int i = 0; i < nums.size(); i++){
+            if(hmap[nums[i]] > nums.size()/3 && hset.find(nums[i]) == hset.end()){
+                ret.push_back(nums[i]);
+                hset.insert(nums[i]);
+            }
+        }
+
+        return ret;
+    }
+};
